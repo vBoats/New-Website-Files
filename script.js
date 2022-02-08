9a324dabd174d99c9e3b793b96a7cd5bc7705fa0
vBoats <rsmc03@gmail.com>
2022-02-08 11:58:59 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+function displayResults(answer) {
+  var pic;
+  if (answer == 0) {
+    pic = "https://www.biography.com/.image/t_share/MTE1ODA0OTcxNzMyNjY1ODY5/wolfgang-mozart-9417115-2-402.jpg"
+  }
+  else {
+    pic = "https://upload.wikimedia.org/wikipedia/commons/6/6f/Beethoven.jpg"
+  }
+  document.getElementById('myImage').src = pic;
+}
+
+
+function changeColor(btn) {
+
+}
