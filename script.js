634eca42173708cecc4aa6b79fb3af91f5ebbf73
vBoats <rsmc03@gmail.com>
2022-03-01 11:50:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-function displayResults(answer) {
-  var pic;
-  if (answer == 0) {
-    pic = "https://www.biography.com/.image/t_share/MTE1ODA0OTcxNzMyNjY1ODY5/wolfgang-mozart-9417115-2-402.jpg"
+const button = document.querySelector('input');
+const paragraph = document.querySelector('p');
+
+button.addEventListener('click', updateButton);
+
+function updateButton() {
+  if (button.value === 'Submit') {
+    button.value = 'Unsubmit';
+    if(paragraph.id === 'recommendation') {
+      paragrpah.textContent = 'This is the recommendation';
+    }
+
   }
-  else {
-    pic = "https://upload.wikimedia.org/wikipedia/commons/6/6f/Beethoven.jpg"
-  }
-  document.getElementById('myImage').src = pic;
-}
-
-
-function changeColor(btn) {
-
 }
